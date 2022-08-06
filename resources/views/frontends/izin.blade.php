6a9baef2c41f70a9cafa324162fcfe0bacf6f296
ultramenid <malichamdan@gmail.com>
2022-08-07 04:28:57 +0700

    new padding

--- resources/views/frontends/izin.blade.php
+++ resources/views/frontends/izin.blade.php
@@ -5,7 +5,7 @@
         @include('partials.navMobile')
         @include('partials.nav')
          <div class="absolute w-full bg-pabrik">
-            <iframe style="height:100vh; width:100%;" src="https://datastudio.google.com/embed/reporting/4028590d-f7fc-4c97-a226-d847e7fe492d/page/XdVvC"  frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="sm:mt-12 mt-0 py-12" style="height:100vh; width:100%;" src="https://datastudio.google.com/embed/reporting/4028590d-f7fc-4c97-a226-d847e7fe492d/page/XdVvC"  frameborder="0" style="border:0" allowfullscreen></iframe>
             <footer class="w-full bg-gray-100 py-2  z-50">
                 <div class="mx-auto max-w-7xl flex flex-col justify-center items-center text-center px-4 py-4">
                     <div class="flex items-center space-x-2">
