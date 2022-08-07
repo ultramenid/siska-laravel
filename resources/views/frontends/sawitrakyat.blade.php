02a13cd6790edec38ce38e8eb4b332ffebe869f3
ultramenid <malichamdan@gmail.com>
2022-08-07 14:25:37 +0700

    change sawitrakyar url

--- resources/views/frontends/sawitrakyat.blade.php
+++ resources/views/frontends/sawitrakyat.blade.php
@@ -5,7 +5,7 @@
         @include('partials.navMobile')
         @include('partials.nav')
          <div class="absolute w-full bg-pabrik">
-            <iframe class="py-12 sm:mt-12 mt-0" style="height:100vh; width:100%;" src="https://datastudio.google.com/embed/reporting/52470773-da14-4cdc-b8e5-4b337f8c8612/page/PuQxC"  frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="py-12 sm:mt-12 mt-0" style="height:100vh; width:100%;" src="https://datastudio.google.com/embed/reporting/fddbcd6c-d607-47f8-ad85-8ac09679703b/page/5yczC"  frameborder="0" style="border:0" allowfullscreen></iframe>
             <footer class="w-full bg-gray-100 py-2  z-50">
                 <div class="mx-auto max-w-7xl flex flex-col justify-center items-center text-center px-4 py-4">
                     <div class="flex items-center space-x-2">
