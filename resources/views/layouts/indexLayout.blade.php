7914a7740c5a374f677800a31bbae70b473c21fa
ultramenid <malichamdan@gmail.com>
2022-08-26 18:56:13 +0700

    changelivewire

--- resources/views/layouts/indexLayout.blade.php
+++ resources/views/layouts/indexLayout.blade.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
-    @livewireStyles
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+
     <script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css">
     <link href="css/Control.MiniMap.min.css" rel="stylesheet">
