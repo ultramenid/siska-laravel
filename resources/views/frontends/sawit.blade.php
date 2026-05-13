2018c555bb2a747bd1fb0b9e8d87faa39e6a3015
ultramenid <malichamdan@gmail.com>
2026-05-13 08:16:55 +0700

    redesign: dashboard sawit as single scrollable page with all sections

--- resources/views/frontends/sawit.blade.php
+++ resources/views/frontends/sawit.blade.php
@@ -13,73 +13,64 @@
             </div>
         </div>
 
-        {{-- Tabs --}}
-        <div class="max-w-7xl mx-auto px-6 py-10" x-data="{ tab: 'pengusahaan', mutasi: 'pbs' }">
-
-            {{-- Tab Nav --}}
-            <div class="flex flex-wrap gap-2 mb-8 border-b border-gray-200 pb-4">
-                @foreach ([
-                    'pengusahaan'     => 'Pengusahaan',
-                    'mutasi'          => 'Mutasi Tanaman',
-                    'perkebunanbesar' => 'Perkebunan Besar',
-                    'perkebunanrakyat'=> 'Perkebunan Rakyat',
-                    'produksi'        => 'Produksi',
-                    'pabrik'          => 'Pabrik',
-                ] as $key => $label)
-                <button
-                    @click="tab = '{{ $key }}'"
-                    :class="tab === '{{ $key }}' ? 'text-white font-semibold' : 'text-gray-600 hover:text-gray-900 hover:bg-gray-100'"
-                    class="px-4 py-2 rounded-lg text-sm transition-colors duration-150 focus:outline-none"
-                    :style="tab === '{{ $key }}' ? 'background-color: #009180;' : ''"
-                >{{ $label }}</button>
-                @endforeach
-            </div>
+        <div class="max-w-7xl mx-auto px-6 py-12 space-y-16">
 
             {{-- Pengusahaan --}}
-            <div x-show="tab === 'pengusahaan'" x-transition>
-                <h2 class="text-xl font-semibold text-gray-900 mb-6">Pengusahaan</h2>
+            <div>
+                <h2 class="text-xl font-semibold text-gray-900 mb-2">Pengusahaan</h2>
+                <div class="w-10 h-1 mb-6" style="background-color: #009180;"></div>
                 <div id="chart-pengusahaan"></div>
             </div>
 
+            <hr class="border-gray-200">
+
             {{-- Mutasi Tanaman --}}
-            <div x-show="tab === 'mutasi'" x-transition>
-                <div class="flex items-center justify-between mb-6">
-                    <h2 class="text-xl font-semibold text-gray-900">Mutasi Tanaman</h2>
-                    <div class="flex gap-2">
-                        <button
-                            @click="mutasi = 'pbs'"
-                            :class="mutasi === 'pbs' ? 'text-white font-semibold' : 'text-gray-600 hover:bg-gray-100'"
-                            class="px-3 py-1.5 rounded-lg text-sm transition-colors duration-150 focus:outline-none"
-                            :style="mutasi === 'pbs' ? 'background-color: #009180;' : ''"
-                        >Perkebunan Besar Swasta</button>
-                        <button
-                            @click="mutasi = 'pbr'"
-                            :class="mutasi === 'pbr' ? 'text-white font-semibold' : 'text-gray-600 hover:bg-gray-100'"
-                            class="px-3 py-1.5 rounded-lg text-sm transition-colors duration-150 focus:outline-none"
-                            :style="mutasi === 'pbr' ? 'background-color: #009180;' : ''"
-                        >Perkebunan Rakyat</button>
-                    </div>
+            <div x-data="{ mutasi: 'pbs' }">
+                <h2 class="text-xl font-semibold text-gray-900 mb-2">Mutasi Tanaman</h2>
+                <div class="w-10 h-1 mb-6" style="background-color: #009180;"></div>
+                <div class="flex gap-2 mb-6">
+                    <button
+                        @click="mutasi = 'pbs'"
+                        :class="mutasi === 'pbs' ? 'text-white font-semibold' : 'text-gray-600 hover:bg-gray-100'"
+                        class="px-3 py-1.5 rounded-lg text-sm transition-colors duration-150 focus:outline-none"
+                        :style="mutasi === 'pbs' ? 'background-color: #009180;' : ''"
+                    >Perkebunan Besar Swasta</button>
+                    <button
+                        @click="mutasi = 'pbr'"
+                        :class="mutasi === 'pbr' ? 'text-white font-semibold' : 'text-gray-600 hover:bg-gray-100'"
+                        class="px-3 py-1.5 rounded-lg text-sm transition-colors duration-150 focus:outline-none"
+                        :style="mutasi === 'pbr' ? 'background-color: #009180;' : ''"
+                    >Perkebunan Rakyat</button>
                 </div>
                 <div x-show="mutasi === 'pbs'" id="chart-mutasi-pbs"></div>
                 <div x-show="mutasi === 'pbr'" id="chart-mutasi-pbr"></div>
             </div>
 
+            <hr class="border-gray-200">
+
             {{-- Perkebunan Besar --}}
-            <div x-show="tab === 'perkebunanbesar'" x-transition>
-                <h2 class="text-xl font-semibold text-gray-900 mb-6">Perkebunan Besar Swasta</h2>
+            <div>
+                <h2 class="text-xl font-semibold text-gray-900 mb-2">Perkebunan Besar Swasta</h2>
+                <div class="w-10 h-1 mb-6" style="background-color: #009180;"></div>
                 <div id="chart-perkebunanbesar"></div>
             </div>
 
+            <hr class="border-gray-200">
+
             {{-- Perkebunan Rakyat --}}
-            <div x-show="tab === 'perkebunanrakyat'" x-transition>
-                <h2 class="text-xl font-semibold text-gray-900 mb-6">Perkebunan Rakyat</h2>
+            <div>
+                <h2 class="text-xl font-semibold text-gray-900 mb-2">Perkebunan Rakyat</h2>
+                <div class="w-10 h-1 mb-6" style="background-color: #009180;"></div>
                 <div id="chart-perkebunanrakyat"></div>
             </div>
 
+            <hr class="border-gray-200">
+
             {{-- Produksi --}}
-            <div x-show="tab === 'produksi'" x-transition>
-                <h2 class="text-xl font-semibold text-gray-900 mb-6">Produksi</h2>
-                <div class="flex flex-col items-center justify-center py-24 text-center">
+            <div>
+                <h2 class="text-xl font-semibold text-gray-900 mb-2">Produksi</h2>
+                <div class="w-10 h-1 mb-6" style="background-color: #009180;"></div>
+                <div class="flex flex-col items-center justify-center py-16 text-center">
                     <div class="w-16 h-16 rounded-full flex items-center justify-center mb-4" style="background-color: #e6f4f2;">
                         <svg class="w-8 h-8" style="color: #009180;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
@@ -90,9 +81,12 @@
                 </div>
             </div>
 
+            <hr class="border-gray-200">
+
             {{-- Pabrik --}}
-            <div x-show="tab === 'pabrik'" x-transition>
-                <h2 class="text-xl font-semibold text-gray-900 mb-6">Pabrik</h2>
+            <div>
+                <h2 class="text-xl font-semibold text-gray-900 mb-2">Pabrik</h2>
+                <div class="w-10 h-1 mb-6" style="background-color: #009180;"></div>
                 <iframe style="height: 80vh; width: 100%;" src="https://datastudio.google.com/embed/reporting/d72b87aa-45b2-48c2-b9e2-315f98e5e41a/page/rTMvC" frameborder="0" allowfullscreen class="rounded-xl border border-gray-200"></iframe>
             </div>
 
@@ -104,10 +98,10 @@
 
 @push('script')
 <script>
-    var pengusahaanPBS = JSON.parse('<?php echo $pengusahaanPBS ?>');
-    var pengusahaanPBR = JSON.parse('<?php echo $pengusahaanPBR ?>');
-    var mutasiPBS      = JSON.parse('<?php echo $pbs ?>');
-    var mutasiPBR      = JSON.parse('<?php echo $pbr ?>');
+    var pengusahaanPBS   = JSON.parse('<?php echo $pengusahaanPBS ?>');
+    var pengusahaanPBR   = JSON.parse('<?php echo $pengusahaanPBR ?>');
+    var mutasiPBS        = JSON.parse('<?php echo $pbs ?>');
+    var mutasiPBR        = JSON.parse('<?php echo $pbr ?>');
     var perkebunanbesar  = JSON.parse('<?php echo $perkebunanbesar ?>');
     var perkebunanrakyat = JSON.parse('<?php echo $perkebunanrakyat ?>');
 
